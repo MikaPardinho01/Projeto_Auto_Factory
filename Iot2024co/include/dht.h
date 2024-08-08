96ac12c295f9f65e071b4f03c851d6aaffd00eb7
Mikaelly Pardinho <168122231+MikaPardinho01@users.noreply.github.com>
2024-08-08 17:08:37 -0300

    Alterações
    Foram alterados os codigos

---
+++ Iot2024co/include/dht.h
@@ -0,0 +1,5 @@
+void dht_umidade();
+void dht_temperatura();
+float lerTemperatura(void);
+float lerUmidade(void);
+void setup_dht();
--- Iot2024co/include/iot.h
+++ Iot2024co/include/iot.h
@@ -4,4 +4,5 @@
 void setup_wifi();
 void inicializa_mqtt();
 void publica_mqtt(String topico, String msg);
+void publica_mqtt(String topico, String msgm);
 void atualiza_mqtt();
