8165667a73e8bc5fbf46bbefa8f8ec7dba3c1c51
KaueAmbrosiom <kauehxzx@gmail.com>
2024-08-23 14:54:48 -0300

    aletara;ao kaue

--- Esteira/src/main.cpp
+++ Esteira/src/main.cpp
@@ -4,6 +4,8 @@
 
 const int stepsPerRevolution = 2048;  // é o número de passos que um motor de passo deve dar para completar uma volta completa de 360 graus.
 // Bom dia
+
+// Bom dia kaue
 // *===== DEFINICOES ======
 // Pins setados no motor de passo
 #define IN1 19
